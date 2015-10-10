5e485d5a94987de4c399af5b456a9b951cf3e424
Alexander Dayneko <dayneko.alexander@gmail.com>
2015-10-10 20:24:00 +0300

    Renaming InsertNode Method

--- JS Training/BinarySearchTree.js
+++ JS Training/BinarySearchTree.js
@@ -22,30 +22,31 @@ BinarySearchTree.prototype.GetNode = function(data) {
 	return get(node);
 }
 
-BinarySearchTree.prototype.InsertNode = function(data) {
+BinarySearchTree.prototype.AddNode = function(data) {
 	for(var i = 0; i < arguments.length; i++) {
 		if(this.root === null)
 			this.root = new Node(arguments[i]);
 		else {
 			var data = arguments[i];
-			(function insertNode(currNode){
+			(function addNode(currNode){
 				if(data < currNode.data){
 					if(currNode.left === null)
 						currNode.left = new Node(data, currNode);
 					else
-						insertNode(currNode.left);
+						addNode(currNode.left);
 				}
 				else {
 					if(currNode.right === null)
 						currNode.right = new Node(data, currNode);
 					else
-						insertNode(currNode.right)
+						addNode(currNode.right)
 				}
 			})(this.root);
 		}
 	}
 	return this;
 };
+
 BinarySearchTree.prototype.GetMax = function() {
 	var node = this.root;
 	while(node.right != null)
@@ -60,6 +61,6 @@ BinarySearchTree.prototype.GetMin = function() {
 	console.log("Min tree element: " + node.data);
 }	
 var tree = new BinarySearchTree();
-tree.InsertNode(20,8,25,7,11,3,9,10)
+tree.AddNode(20,8,25,7,11,3,9,10)
 tree.GetMax();
 tree.GetMin();
