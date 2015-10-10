66a46d081fdff40a9b398f7a20afc87dac40b4a6
Alexander Dayneko <dayneko.alexander@gmail.com>
2015-10-10 20:19:42 +0300

    Adding InsertNode method

--- JS Training/BinarySearchTree.js
+++ JS Training/BinarySearchTree.js
@@ -4,11 +4,11 @@ function Node(data, parent) {
 	this.left = null;
 	this.right = null;
 	return this;
-}
+};
 
 function BinarySearchTree() {
 	this.root = null;
-}
+};
 
 BinarySearchTree.prototype.GetNode = function(data) {
 	var node = this.root;
@@ -21,3 +21,45 @@ BinarySearchTree.prototype.GetNode = function(data) {
 	}
 	return get(node);
 }
+
+BinarySearchTree.prototype.InsertNode = function(data) {
+	for(var i = 0; i < arguments.length; i++) {
+		if(this.root === null)
+			this.root = new Node(arguments[i]);
+		else {
+			var data = arguments[i];
+			(function insertNode(currNode){
+				if(data < currNode.data){
+					if(currNode.left === null)
+						currNode.left = new Node(data, currNode);
+					else
+						insertNode(currNode.left);
+				}
+				else {
+					if(currNode.right === null)
+						currNode.right = new Node(data, currNode);
+					else
+						insertNode(currNode.right)
+				}
+			})(this.root);
+		}
+	}
+	return this;
+};
+BinarySearchTree.prototype.GetMax = function() {
+	var node = this.root;
+	while(node.right != null)
+		node = node.right;
+	console.log("Max tree element: " + node.data);
+}
+
+BinarySearchTree.prototype.GetMin = function() {
+	var node = this.root;
+	while(node.left != null)
+		node = node.left;
+	console.log("Min tree element: " + node.data);
+}	
+var tree = new BinarySearchTree();
+tree.InsertNode(20,8,25,7,11,3,9,10)
+tree.GetMax();
+tree.GetMin();
