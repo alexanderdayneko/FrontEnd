95afb52cea0de9c4094becbb0558b54c5502b1b9
Alexander Dayneko <dayneko.alexander@gmail.com>
2015-10-10 18:58:29 +0300

    Adding GetNode method

--- JS Training/BinarySearchTree.js
+++ JS Training/BinarySearchTree.js
@@ -1,7 +1,11 @@
-var Node = function(data,parent){
+function Node(data, parent) {
 	this.data = data;
 	this.parent = parent;
 	this.left = null;
 	this.right = null;
 	return this;
+}
+
+function BinarySearchTree() {
+	this.root = null;
 }
